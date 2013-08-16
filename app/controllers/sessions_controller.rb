c78b79b8427b764ad43cecf6505371ff9c433487
Ryan Rusnak <ryandrusnak@gmail.com>
2013-08-16 13:18:01 -0400

    uploading repo

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,43 @@
+class SessionsController < ApplicationController
+  # def create
+  #    auth = request.env["omniauth.auth"]
+  #    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+  #    session[:user_id] = user.id
+  #    redirect_to root_url, :notice => "Signed in!"
+  #  end
+
+  #  def destroy
+  #    session[:user_id] = nil
+  #    redirect_to root_url, :notice => "Signed out!"
+  #  end
+  def new
+    redirect_to '/auth/twitter'
+  end
+
+
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'], 
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+# Reset the session after successful login, per
+# 2.8 Session Fixation – Countermeasures:
+# http://guides.rubyonrails.org/security.html#session-fixation-countermeasures
+    reset_session
+    session[:user_id] = user.id
+    if user.email.blank?
+      redirect_to edit_user_path(user), :alert => "Please enter your email address."
+    else
+      redirect_to root_url, :notice => 'Signed in!'
+    end
+
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, :notice => 'Signed out!'
+  end
+
+  def failure
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travel::Application.config.secret_token = '7c088e0189c5b92fb86d685e69b5a91e29b9071a11867cf93a7d088ce1918dadd028fb3d63e56441c6e528fcc5b4d9c4ff40db6d819c67cab0e1bdb29b385444'
